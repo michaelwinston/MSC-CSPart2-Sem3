5ffe14079131cf921ef3c8d473c42ea1d8fde2b1
michaelwinston <90085394+michaelwinston@users.noreply.github.com>
2022-10-06 21:58:27 +0530

    Add files via upload

---
+++ CFL/Mini Project/test_db.2022-09-29.sql
@@ -0,0 +1,16 @@
+
+
+CREATE TABLE `employee` (
+  `Name` varchar(20) NOT NULL,
+  `Age` int(3) NOT NULL,
+  `Phone` int(10) NOT NULL,
+  `Address` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO employee VALUES
+("John","20","1234567890","Sion"),
+("Alice","22","937652312","Matunga"),
+("Michael","22","837472491","Ghatkophar");
+
+
